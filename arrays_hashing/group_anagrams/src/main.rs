164c5af6afa02b8613908e47928875649fb6b547
Adao <anthonydaoy@gmail.com>
2025-07-10 21:47:23 -0700

    started group anagrams, completed planning, next is implementation.

---
+++ arrays_hashing/group_anagrams/src/main.rs
@@ -0,0 +1,18 @@
+use std::collections::HashMap;
+
+struct GroupAnagrams; 
+impl GroupAnagrams {
+    fn group_words (strs: Vec<String>) -> Vec<Vec<String>> {
+        let mut map: HashMap<Vec<i32>, String>> = HashMap::new(); 
+
+        for word in strs {
+            let mut array = [0; 26];
+        } 
+    }
+}
+
+fn main() {
+    let str1 = vec!("act", "pots", "tops", "cat", "stop", "hat");
+    let str2 = vec!("x");
+    let str3 = vec!(""); 
+}
