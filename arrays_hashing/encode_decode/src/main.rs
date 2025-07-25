557620669850e5c701f0a3320c6644ba1120461a
Adao <anthonydaoy@gmail.com>
2025-07-25 14:03:11 -0700

    finished encode/decode

--- arrays_hashing/encode_decode/src/main.rs
+++ arrays_hashing/encode_decode/src/main.rs
@@ -1,6 +1,6 @@
 struct Solution;
 impl Solution {
-    fn encode(strs:& Vec<Vec<char>>) -> Vec<char> {
+    fn encode(strs: & Vec<Vec<char>>) -> Vec<char> {  
         let mut encoded_str: Vec<char> = Vec::new(); 
         for str in strs.iter() {
             encoded_str.push(char::from_digit(str.len() as u32, 10).unwrap());
@@ -13,14 +13,41 @@ impl Solution {
         encoded_str
     }
 
-    fn decode(strs: Vec<char>) -> Vec<Vec<char>> {
-        todo!(); 
+    fn decode(strs: & Vec<char>) -> Vec<String> {
+        let mut decode_str: Vec<String> = Vec::new(); 
+        let mut i = 0; 
+        while i < strs.len() - 1 {
+            if strs[i].is_digit(10) && strs[i + 1] == '#' {
+                let mut decode = Vec::new(); 
+                let mut start = i + 2;
+                let mut counter = strs[i].to_digit(10).unwrap();
+                
+                while counter != 0 {
+                    decode.push(strs[start]);
+                    start += 1;  
+                    counter -= 1; 
+                }
+                i = start; 
+                decode_str.push(decode.into_iter().collect());
+            }   
+        }
 
+        decode_str
     }
 }
 
 fn main() {
+    // Example 1:
     let input: Vec<Vec<char>> = vec!["neet".chars().collect(), "code".chars().collect(), "love".chars().collect(), "you".chars().collect()];
+    let encoded = Solution::encode(&input); 
+    let decoded = Solution::decode(&encoded);
+    println!("Encoded String for Example 1:\n {:?}", encoded);
+    println!("Decoded Strings for Example 2:\n {:?}", decoded);
     
-    println!("{:?}", Solution::encode(&input));
+    // Example 2: 
+    let input = vec!["we".chars().collect(), "say".chars().collect(), ":".chars().collect(), "yes".chars().collect()];
+    let encoded = Solution::encode(&input);
+    let decoded = Solution::decode(&encoded);
+    println!("Encoded String for Example 2:\n {:?}", encoded);
+    println!("Decoded Strings for Example 2:\n {:?}", decoded);
 }
