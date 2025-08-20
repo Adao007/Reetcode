5177faefbc2ffb2a3a4cfa7297a9d56e5f7d179d
Tony <anthonydaoy@gmail.com>
2025-08-20 12:11:03 -0700

    started two pointer problem two int sum

---
+++ two_pointer/two_int_sum/src/main.rs
@@ -0,0 +1,13 @@
+struct Solution; 
+impl Solution {
+    fn get_two_sum(nums: &Vec<i32>, target: &i32) -> Vec<i32> {
+        
+    }
+}
+
+fn main() {
+    let numbers = vec![1, 2, 3, 4];
+    let target = 3; 
+
+    assert_eq!(Solution::get_two_sum(&numbers, &target), target);
+}
