838260fce8ba3a69e1d586fc8a84de4e06c50738
Anthony Dao <anthonydaoy@gmail.com>
2026-02-27 00:10:29 -0800

    finished csv example problem

--- Python_Mill/salesmen_script/best_salesmen.py
+++ Python_Mill/salesmen_script/best_salesmen.py
@@ -23,13 +23,74 @@ def make_salesmen_dictionary():
 
 def add_annual_goal(result):
     with open('annual_goals.csv', newline='') as f: 
-        reader = csv.reader(f, delimit=',')
+        reader = csv.reader(f, delimiter=',')
         for row in reader:
-            if row[1] in result: 
+            if row[0] in result: 
+                result[row[0]]['annual_goal'] = int(row[2]) * 4
+    
+    return result
+
+def add_percentage_of_sales(result):
+    list = []
+    with open('sales_record.csv', newline='') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader: 
+            list.append(row)
+
+    list = modify_list(list)
+
+    for entry in list: 
+        if entry[4] == 'closed':
+            key = entry[0]
+            if key in result:
+                if 'percentage' in result[key]:
+                    result[key]['percentage'] += int(entry[3])
+                else:
+                    result[key]['percentage'] = int(entry[3])
+
+    for i in result:
+        if 'percentage' in result[i]:
+            result[i]['percentage'] = int(result[i]['percentage']) / int(result[i]['annual_goal'])
+
+    return result
+
+def modify_list(list):
+    for entry in list: 
+        match entry[2]:
+            case 'Jupi':
+                entry[0] = '1'
+            case 'Gabe':
+                entry[0] = '3'
+            case 'Tony':
+                entry[0] = '4'
+            case 'Azalea':
+                entry[0] = '7'
+            case 'Clara':
+                entry[0] = '8'
+            case _: 
+                print("Not valid entry/Not sales employee!")
+
+    return list
+
+def get_best_salesmen(result):
+    list = []
+    for entry in result:
+        if 'percentage' in result[entry]:
+            list.append((result[entry]['percentage'], entry))
+
+    sorted_list = sorted(list)
+    return sorted_list
+
+
 
 def main(): 
     test = make_salesmen_dictionary()
-    print(test)
+    test = add_annual_goal(test)
+    test = add_percentage_of_sales(test)
+    order = get_best_salesmen(test)
+    
+    for position in reversed(order): 
+        print(test[(position[1])])
 
 
 if __name__ == '__main__':
